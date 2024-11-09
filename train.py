83711825b40a70b7a78b8bb7fac5d2cfd08497f1
d3ac <1048563083@qq.com>
2024-11-09 16:48:49 +0800

    add plt

--- train.py
+++ train.py
@@ -6,6 +6,7 @@ import torchvision.transforms as transforms
 from model import VAE, Encoder, Decoder
 import tqdm
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 if __name__ == "__main__":
@@ -13,7 +14,7 @@ if __name__ == "__main__":
     latent_dim = 64
     data_dim = 784
     batch_size = 128
-    epochs = 20
+    epochs = 200
 
     # load data
     train_iter = DataLoader(datasets.MNIST(root="./data", train=True, download=True, transform=transforms.ToTensor()), batch_size=batch_size, shuffle=True, num_workers=7, pin_memory=True, drop_last=True)
@@ -23,10 +24,12 @@ if __name__ == "__main__":
     encoder = Encoder(latent_dim, data_dim).to(torch.device('cuda'))
     decoder = Decoder(latent_dim, data_dim).to(torch.device('cuda'))
     model = VAE(encoder, decoder).to(torch.device('cuda'))
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=1e-4)
+    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[50, 100, 150], gamma=0.5)
 
     # training
     Trange = tqdm.trange(epochs)
+    loss_list = []
     for epoch in Trange:
         model.train()
         for X, y in train_iter:
@@ -35,7 +38,8 @@ if __name__ == "__main__":
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-        
+        scheduler.step()
+
         model.eval()
         test_loss = 0
         with torch.no_grad():
@@ -46,3 +50,9 @@ if __name__ == "__main__":
         
         test_loss /= len(test_iter)
         Trange.set_postfix(test_loss=test_loss)
+        loss_list.append(test_loss)
+    
+
+
+    plt.plot(np.arange(len(loss_list)), np.array(loss_list))
+    plt.show()
